7a8a8b4c047f5738dfd9c4e13babeb7f2d9150ca
mlresearchteam33 <ml.research.team33@gmail.com>
2026-05-11 16:51:36 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ import numpy as np
 from PIL import Image
 import tensorflow as tf
 import traceback
+import os
 
 # =========================
 # FLASK APP
@@ -123,9 +124,10 @@ def predict():
 
 if __name__ == "__main__":
 
+    port = int(os.environ.get("PORT", 5000))
+
     app.run(
         host="0.0.0.0",
-        port=5000,
-        debug=True,
-        use_reloader=False
+        port=port,
+        debug=False
     )
